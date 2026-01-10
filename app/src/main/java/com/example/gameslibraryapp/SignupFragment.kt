50705f8add5b0938cc475c9981bb0ec6a0bbc8f6
irakli <irakli.kasradze.1@btu.edu.ge>
2026-01-10 22:38:00 +0400

    add sign up form validation

--- app/src/main/java/com/example/gameslibraryapp/SignupFragment.kt
+++ app/src/main/java/com/example/gameslibraryapp/SignupFragment.kt
@@ -1,18 +1,35 @@
 package com.example.gameslibraryapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.gameslibraryapp.databinding.FragmentSignupBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 
 class SignupFragment : Fragment() {
 
+    private lateinit var auth: FirebaseAuth;
     private var _binding: FragmentSignupBinding? = null
     private val binding get() = _binding!!
+
+    private var isEmailValid: Boolean = false
+    private var isUsernameValid: Boolean = false
+    private var isPasswordValid: Boolean = false
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,11 +42,95 @@ class SignupFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.emailInput.addTextChangedListener(object : SimpleTextWatcher() {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val email = binding.emailInput.text.toString().trim()
+
+                isEmailValid =
+                    email.isNotEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(email)
+                        .matches()
+
+                if (!isEmailValid) {
+                    binding.emailLayout.error = "Enter a valid email address"
+                } else {
+                    binding.emailLayout.error = null
+                }
+            }
+        })
+
+        binding.usernameInput.addTextChangedListener(object : SimpleTextWatcher() {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().length > 2 && s.toString().isNotBlank()) {
+                    binding.usernameLayout.error = null
+                    isUsernameValid = true
+                } else {
+                    binding.usernameLayout.error = "Username should use at least 3 letters"
+                    isUsernameValid = false
+                }
+            }
+        })
+
+        val passwordWatcher = object : SimpleTextWatcher() {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                isPasswordValid = validatePasswords()
+            }
+        }
+
+        binding.passwordInput.addTextChangedListener(passwordWatcher)
+        binding.confirmPasswordInput.addTextChangedListener(passwordWatcher)
+
+
         binding.haveAnAccountBtn.setOnClickListener {
 
             findNavController().navigate(R.id.action_signupFragment_to_loginFragment)
         }
+
+        binding.signupBtn.isEnabled = isEmailValid && isUsernameValid && isPasswordValid
+
+        binding.signupBtn.setOnClickListener {
+            Toast.makeText(
+                context,
+                "Validation successful! Creating account...",
+                Toast.LENGTH_SHORT
+            ).show()
+
+        }
     }
+
+    fun validatePasswords(): Boolean {
+        val password = binding.passwordInput.text.toString().trim()
+        val confirmPassword = binding.confirmPasswordInput.text.toString().trim()
+        var passwordValid = false
+        var confirmPasswordValid = false
+
+        if (password.length < 6) {
+            binding.passwordLayout.error = "Password must be at least 6 characters"
+        } else {
+            binding.passwordLayout.error = null
+            passwordValid = true
+        }
+
+        if (confirmPassword.isNotEmpty() && password != confirmPassword) {
+            binding.confirmPasswordLayout.error = "Passwords do not match"
+        } else {
+            binding.confirmPasswordLayout.error = null
+            confirmPasswordValid = true
+        }
+        return passwordValid && confirmPasswordValid
+    }
+
+
+    private abstract class SimpleTextWatcher : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
